1ea173c66fb8ab661025cbffb4dc8e512065ab46
Aviadoor <diego.2003.29.12@gmail.com>
2025-04-08 20:44:38 -0500

    se implemento el metodo Agregar Cliente

--- src/main/java/com/mycompany/factuya/FactuYa.java
+++ src/main/java/com/mycompany/factuya/FactuYa.java
@@ -9,8 +9,15 @@ package com.mycompany.factuya;
  * @author Jhon
  */
 public class FactuYa {
-
+    public FactuYa(){
+        
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        FactuYa factura = new FactuYa();
+        factura.AgregarCliente("Pedro");
+    }
+    public void AgregarCliente(String nombre){
+        System.out.print("Cliente agregado: " + nombre);
     }
 }
